4701d6aa831de6404557e080c7f7dccfb7cb4d16
CannoHarito <45878603+CannoHarito@users.noreply.github.com>
2017-10-18 23:54:40 +0900

    ver.0.00.4 バグ修正+UI調整 1曲リピート時の動作を修正

--- ikara_poi.js
+++ ikara_poi.js
@@ -1,5 +1,5 @@
-/*ikara_poi.js iKaraっぽい。ver.0.00.3 詳細はikara_poi.htmlを参照*/
-var state=false, ie, dom_description, dom_lyrics, iTunes, fso, currenttrack_id;
+/*ikara_poi.js iKaraっぽい。ver.0.00.4 詳細はikara_poi.htmlを参照*/
+var state=false, ie, dom_description, dom_lyrics, iTunes, fso, currenttrackId;
 var html="ikara_poi.html";
 var lyricmaster = false;
 lyricmaster = 'C:\\Windows\\System32\\wscript.exe "C:\\Program_Free\\Lyrics Master\\ExtSupport.js" multi "[title]" "[artist]"';
@@ -20,19 +20,24 @@ function ITEvent_OnAboutToPromptUserToQuitEvent(){
 */
 function display_lyrics(track){
     if(!track){
-        dom_lyrics.innerHTML = "No lyrics";
+        //dom_lyrics.innerHTML = "曲を再生してください";
         return;
     }
+  try{
     dom_description.innerHTML = "<ul><li class='name'>"+track.Name+"</li>"
         +"<li class='artist'>"+track.Artist+"</li>"
         +"<li class='album'>"+track.Album+"</li></ul>";
     dom_lyrics.innerHTML= track.Lyrics.replace(/\r/g,"<br>");
-    currenttrack_id = track.TrackID;
+    currenttrackId = track.TrackID;
     var newScript=ie.document.createElement("script");
     newScript.type = "text/javascript";
     newScript.text = "startScroll("+track.Lyrics.split(/\r/).length+","+track.Duration+","+iTunes.PlayerPosition+");";
 //    WScript.Echo(newScript.text);
     dom_lyrics.appendChild(newScript);
+  }catch(e){
+    if(e.message=="The track has been deleted.")return;//smartplaylistでまれに起こる
+    throw e;
+  }
 }
 function IE_BeforeQuit(){
     state=false;
@@ -50,6 +55,7 @@ html = fso.BuildPath(fso.GetParentFolderName(WScript.ScriptFullName) ,html);
 ie = WScript.CreateObject("InternetExplorer.Application");
 ie.Navigate(html);
 waitIE();
+ie.MenuBar=ie.ToolBar=ie.AddressBar=0;
 dom_description = ie.document.getElementById("description");
 dom_lyrics = ie.document.getElementById("lyrics");
 if(lyricmaster){
@@ -63,9 +69,8 @@ if(lyricmaster){
 //iTunes = WScript.CreateObject("iTunes.Application","ITEvent_");//iTuens12.7よりイベント接続廃止
 iTunes = WScript.CreateObject("iTunes.Application");
 ie.document.body.onbeforeunload = IE_BeforeQuit;
-ie.document.onmousedown = function (e){
-    //WScript.Echo("mousedown:"+e.button);
-    if(e.button==1)display_lyrics(iTunes.CurrentTrack);
+ie.document.getElementById("stateManual").onclick = function(){
+    if(iTunes.PlayerState != 0)display_lyrics(iTunes.CurrentTrack);
 };
 if(lyricmaster){
     ie.document.getElementById("lyricmaster").onclick = function(){
@@ -77,24 +82,22 @@ if(lyricmaster){
 }
 ie.Visible = true;
 state=true;
-if(iTunes.PlayerState != 0){
+if(iTunes.PlayerState != 0)
     display_lyrics(iTunes.CurrentTrack);
-}else display_lyrics(false);
-
 
 while(state){
-    if(iTunes.PlayerState != 0 && iTunes.CurrentTrack.TrackID != currenttrack_id)
-      display_lyrics(iTunes.CurrentTrack);
+    if(iTunes.PlayerState != 0 && iTunes.CurrentTrack.TrackID != currenttrackId)
+        display_lyrics(iTunes.CurrentTrack);
     WScript.Sleep(1000);
 }
 if(ie)ie.Quit();
 ie = null;
 iTunes = null;
 } catch (x) {
+    WScript.Echo(x.message);
     try{
         if(ie)ie.quit();
     } catch (x) {
 
     }
-    WScript.Echo(x.message);
 }
